d69b6678a4d484312c85965d3ef38901d8c9c18a
Ozodimgba <idasiadiachi@gmail.com>
2024-01-20 19:47:52 +0000

    push commands

--- bot.ts
+++ bot.ts
@@ -1,33 +1,61 @@
-import TelegramBot from 'node-telegram-bot-api'
-import dotenv from 'dotenv'
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token: any = process.env.TELEGRAM || "";
+const bot = new TelegramBot(token, { polling: true });
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const commands = [
+  { command: 'start', description: 'Start the bot' },
+  { command: 'crank', description: 'Crank a particular market' },
+  { command: 'wallet', description: 'Infomation on your openbook bot wallet' },
+  { command: 'createMarket', description: 'Create market on openbook' },
+  { command: 'placeOrder', description: 'Place order to buy or sell a token on openbook' },
+];
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg: any, match: any) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+// Set the commands
+bot.setMyCommands(commands)
+  .then(() => {
+    console.log('Commands have been set successfully.');
+  })
+  .catch((error) => {
+    console.error('Error setting commands:', error.message);
+  });
 
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  bot.sendMessage(chatId, 'Hello! Welcome to the Open bot.');
+});
 
-  // send back the matched "whatever" to the chat
+bot.onText(/\/crank (.+)/, (msg: any, match: any) => {
+  const chatId = msg.chat.id;
+  console.log(match);
+  const resp = match[1];
+
+  // Send back the matched "whatever" to the chat without the original message
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+bot.onText(/\/wallet (.+)/, (msg: any, match: any) => {
+  const chatId = msg.chat.id;
+  console.log(match);
+  const resp = match[1];
+
+  // Send back the matched "whatever" to the chat without the original message
+  bot.sendMessage(chatId, resp);
+});
+
 bot.on('message', (msg: any) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
+  // Check if the message starts with a slash (/) which indicates a command
+  if (msg.text && msg.text.startsWith('/')) {
+    // Do not send the received message for command messages
+    return;
+  }
+
+  // Send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message', {
     reply_markup: {
       inline_keyboard: [
@@ -42,3 +70,4 @@ bot.on('message', (msg: any) => {
     },
   });
 });
+
